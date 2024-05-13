c28b7875bfacbbe9373111e9f77e0d510b600278
tliu024 <72672078+Huanying1996@users.noreply.github.com>
2024-05-12 17:11:52 -0700

    corrected tailwind usage, aligned nav buttons to the right

--- client/components/NavBar.tsx
+++ client/components/NavBar.tsx
@@ -2,24 +2,24 @@ import React from 'react'
 import Image from 'next/image'
 const NavBar = () => {
   return (
-    <div className='flex p-3 px-10 justify-between border-b-[3px] shawdown-md bg-blue-400'>
-        <div className='flex items-center logo-button-gap'>
+    <div className='flex p-2 px-10 border-b-[3px] justify-between shawdown-md bg-blue-400'>
+        <div className='flex items-center'>
             <Image src='/NavBar Logo.png'
             alt = 'logo' 
             width={100}
-            height={80} className='rounded'/>
-            <div className='hidden md:flex gap-6 items-center justify-end'> {/** this div element should aligh to the right*/}
-                <h2 className='p-1 rounded-medium hover:bg-purple-50
-                cursor-pointer transition-all'>Experience</h2>
-                <h2 className='p-1 rounded-medium hover:bg-purple-50
-                cursor-pointer transition-all'>Filtering Tool</h2>
-                <h2 className='p-1 rounded-medium hover:bg-purple-50
-                cursor-pointer transition-all'>Search</h2>
-                <h2 className='p-1 rounded-medium hover:bg-purple-50
-                cursor-pointer transition-all'>Log in</h2>
-            </div>
+            height={80} className='rounded'/>    
         </div>
         
+        <div className='hidden md:flex gap-6 items-center'> {/** this div element should aligh to the right*/}
+                <h2 className='p-1 rounded-lg hover:bg-purple-100
+                cursor-pointer transition-all'>Experience</h2>
+                <h2 className='p-1 rounded-lg hover:bg-purple-100
+                cursor-pointer transition-all'>Filtering Tool</h2>
+                <h2 className='p-1 rounded-lg hover:bg-purple-100
+                cursor-pointer transition-all'>Search</h2>
+                <h2 className='p-1 rounded-lg hover:bg-purple-100
+                cursor-pointer transition-all'>Log in</h2>
+        </div>
       
     </div>
   )
